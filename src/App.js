55224f068c6838a57908bc1dc09071b9db53bba5
Mariam Mazen <marioumamazen@gmail.com>
2024-09-03 14:21:01 +0300

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './Counter';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>Redux Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
